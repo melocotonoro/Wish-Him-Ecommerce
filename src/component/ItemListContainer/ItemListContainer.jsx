0ae8658fc37e9f62d7c4cbda552ad1cae5bf247f
melocotonoro <melisaoro@gmail.com>
2022-09-07 20:15:03 -0300

    css componente ItemList

--- src/component/ItemListContainer/ItemListContainer.jsx
+++ src/component/ItemListContainer/ItemListContainer.jsx
@@ -7,7 +7,7 @@ const ItemListContainer=()=>{
 
 useEffect(()=>{
 const dataCard=[
-  {id:1,title:"Gorra Yankkes",description:"Gorra camuflada de los New York Yankees",pictureUrl:"https://imagizer.imageshack.com/img924/4809/t72Pu2.jpg",price:2000},
+  {id:1,title:"Gorra Yankes",description:"Gorra camuflada de los NY Yankees",pictureUrl:"https://imagizer.imageshack.com/img924/4809/t72Pu2.jpg",price:2000},
   {id:2,title:"Lentes de sol",description:"Lentes de sol modelo Urban Vulk",pictureUrl:"https://imagizer.imageshack.com/img923/2272/mSxmix.jpg",price:3000},
   {id:3,title:"Reloj deportivo",description:"Reloj deportivo marca Jaguar",pictureUrl:"https://imagizer.imageshack.com/img922/4416/FClWyB.jpg",price:15000},
   {id:4,title:"Remera urbana",description:"Remera urbana marca Marko",pictureUrl:"https://imagizer.imageshack.com/img923/8982/NE5i9h.jpg",price:2500}
