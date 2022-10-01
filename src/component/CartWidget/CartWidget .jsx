037cdd4def2b28c679fdcae3e348bb1dae4206aa
melocotonoro <melisaoro@gmail.com>
2022-09-30 23:45:48 -0300

    modificaciones boton vaciar

--- src/component/CartWidget/CartWidget .jsx
+++ src/component/CartWidget/CartWidget .jsx
@@ -10,17 +10,15 @@ const CartWidget=()=>{
         let unid=countProp()
             return(
                 <div className="containerCart">
-                    <Link to='/cart'>
-                        {/* Condicional que indica si el carrito no tiene productos no aparece, de lo contrario aparece */}
-                        {
-                            unid === 0
-                                ? <div></div>
-                                :<div>
-                                    <img className="shopCart" src='https://res.cloudinary.com/ds7jaqjll/image/upload/v1664333333/Wish-Him/shoppingCart-01_c0m0as.png' alt="carrito compras" />
-                                    <span className='count'>{unid}</span>
-                                </div>
-                        }
-                    </Link>
+                            {/* Condicional que indica si el carrito no tiene productos no aparece, de lo contrario aparece */}
+                            {
+                                unid === 0
+                                    ? <div></div>
+                                    : <Link to='/cart'>
+                                        <img className="shopCart" src='https://res.cloudinary.com/ds7jaqjll/image/upload/v1664333333/Wish-Him/shoppingCart-01_c0m0as.png' alt="carrito compras" />
+                                        <span className='count'>{unid}</span>
+                                    </Link>
+                            }
                 </div>
 )
 }
